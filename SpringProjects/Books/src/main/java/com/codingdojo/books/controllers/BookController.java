b58343a1c32f2b3bfb09fbcfc908225bc758110d
Taher10000 <taheralimakhanawala@gmail.com>
2022-08-13 19:24:29 -0400

    changes

--- SpringProjects/Books/src/main/java/com/codingdojo/books/controllers/BookController.java
+++ SpringProjects/Books/src/main/java/com/codingdojo/books/controllers/BookController.java
@@ -1,10 +1,13 @@
 package com.codingdojo.books.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.codingdojo.books.models.Book;
 import com.codingdojo.books.services.BookService;
@@ -21,4 +24,12 @@ public class BookController {
 		return "show.jsp";
 	}
 	
+
+
+@RequestMapping("/books")
+public String index(Model model) {
+    List<Book> books = bookService.allBooks();
+    model.addAttribute("books", books);
+    return "index.jsp";
+}
 }
